e402410e1dbd94921b774f7e2e48c0e3da0b7e1b
beatriznoe567 <n29871869@gmail.com>
2024-10-02 21:44:08 -0300

    arregle id_venta para que sea autoincremental

--- desarrolloinmobiliario.sql
+++ desarrolloinmobiliario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-09-2024 a las 23:36:04
+-- Tiempo de generación: 01-10-2024 a las 16:07:08
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -33,14 +33,14 @@ CREATE TABLE `vendedores` (
   `Apellido` varchar(50) NOT NULL,
   `Telefono` varchar(70) NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `calificacion` int(11) NOT NULL
+  `Calificacion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `vendedores`
 --
 
-INSERT INTO `vendedores` (`Id_vendedor`, `Nombre`, `Apellido`, `Telefono`, `Email`, `calificacion`) VALUES
+INSERT INTO `vendedores` (`Id_vendedor`, `Nombre`, `Apellido`, `Telefono`, `Email`, `Calificacion`) VALUES
 (1, 'Agustín', 'Castro', '2494678635', 'agustinC@gmail.com', 0),
 (2, 'Pamela ', 'Sosa', '2494582311', 'pamsosa@gmail.com', 0),
 (3, 'Juan', 'Arce', '2494985634', 'ja@gmail.com', 0),
@@ -65,8 +65,10 @@ CREATE TABLE `venta` (
 --
 
 INSERT INTO `venta` (`id_venta`, `inmueble`, `fecha_venta`, `precio`, `Id_vendedor`) VALUES
-(0, 'Casa en las sierras de Tandil.', '2024-09-09', 650000, 1),
-(1, 'Casa de lujo en el  Lago del Fuerte.', '2024-09-11', 550000, 2);
+(1, 'Casa de lujo en el  Lago del Fuerte.', '2024-09-11', 550000, 2),
+(2, 'nueva casa en la sierra', '2024-10-01', 300000, 4),
+(3, 'cabaña cercana al lago, entre sierras', '2024-10-01', 200000, 2),
+(4, 'casa céntrica, inmejorables terminaciones', '2024-10-01', 350000, 1);
 
 --
 -- Índices para tablas volcadas
@@ -94,6 +96,12 @@ ALTER TABLE `venta`
 --
 ALTER TABLE `vendedores`
   MODIFY `Id_vendedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
